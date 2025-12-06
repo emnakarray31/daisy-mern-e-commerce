5842235d1af2894cea25e9f94519b41e8d671946
emnaKarray31 <emna.karray@esprit.tn>
2025-12-06 17:09:17 +0100

    server.js removed frontend for render

--- backend/server.js
+++ backend/server.js
@@ -68,14 +68,14 @@ app.use("/api/chatbot", chatbotRoutes);
   });
 });
 
- if (process.env.NODE_ENV === 'production') {
-  const frontendPath = path.join(__dirname, '../frontend/dist');
-  app.use(express.static(frontendPath));
+//  if (process.env.NODE_ENV === 'production') {
+//   const frontendPath = path.join(__dirname, '../frontend/dist');
+//   app.use(express.static(frontendPath));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(frontendPath, 'index.html'));
-  });
-}
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(frontendPath, 'index.html'));
+//   });
+// }
 
 app.use((err, req, res, next) => {
   console.error('Error:', err);
